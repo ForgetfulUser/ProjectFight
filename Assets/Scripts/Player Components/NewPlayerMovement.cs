928c2a3c3ea41d8142fabbb061fd7eebfbe2dbc7
PhantomNinja <train.trooper@gmail.com>
2026-06-02 15:02:50 -0500

    [Minor] Changed NewPlayerMovement to use CallbackContext and created a playerinput action asset
    CallbackContext is required to most easily implement multiple devices on a shared device, which in turn requires non-projectwide input actions.

--- Assets/Scripts/Player Components/NewPlayerMovement.cs
+++ Assets/Scripts/Player Components/NewPlayerMovement.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+// HeadsUp: You need to use callbackcontext since it makes it easier to handle multiple input devices
+
 public class NewPlayerMovement : MonoBehaviour
 {
     [Header("Player Components")]
@@ -10,77 +12,63 @@ public class NewPlayerMovement : MonoBehaviour
     [SerializeField] Vector3 groundCheckSize;
     [SerializeField] LayerMask groundLayer;
     [Header("Player Actions")]
-    InputAction move;
-    InputAction jump;
-
+ 
     [Header("Player Movement")]
     [SerializeField] float playerSpeed;
     [SerializeField] float jumpForce;
     [SerializeField] float maxFallSpeed;
     bool isGrounded;
     bool isJumping;
-    Vector2 xzMoveVector;
+    Vector2 movementInput;
     float yMoveVector;
     private void Start()
     {
-        move = InputSystem.actions.FindAction("Move");
-        jump = InputSystem.actions.FindAction("Jump");
+
         rb = GetComponent<Rigidbody>();
     }
 
 
     private void Update()
     {
-        xzMoveVector = move.ReadValue<Vector2>(); 
         groundCheck();
-        if (jump.WasPerformedThisFrame() && isGrounded)
-        {
-            playerJump();
-        }
+        
         jumpNegation();
 
 
     }
     private void FixedUpdate()
     {
-        playerMovement();
       
     }
 
     void jumpNegation()
     {
         // Negates player velocity and adds a bit of downward force to make the jump feel less floaty
-        // @@TODO: Have this only be triggered while not falling and apply negative downward velocity, can exploit it to have practically infinite air time by stall
-        if (!isGrounded && rb.linearVelocity.y > 0 && jump.WasReleasedThisDynamicUpdate())
+        // @@TODO: switch this to use callbackcontext 
+        
+        //if (!isGrounded && rb.linearVelocity.y > 0 && jump.WasReleasedThisDynamicUpdate())
+        //{
+        //    rb.linearVelocity = new Vector3(rb.linearVelocity.x, -1f, rb.linearVelocity.z);
+        //}
+    }
+
+
+
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (isGrounded && !isJumping)
         {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, -1f, rb.linearVelocity.z);
+            isJumping = true;
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
         }
     }
 
-    
-    IEnumerator jumpCooldown() // might deprecate this
+    // @@TODO: Add section to add modifiers to change player velocity 
+    public void OnMove(InputAction.CallbackContext context)
     {
-        yield return new WaitForSeconds(2.0f);
-        isJumping = false;
-    }
-
-
-    Vector3 updatePlayerMovement()
-    {
-        Vector3 finalMoveVector;
-        xzMoveVector = move.ReadValue<Vector2>();
-        xzMoveVector = xzMoveVector * playerSpeed;
-        return finalMoveVector = new Vector3(xzMoveVector.x, yMoveVector, xzMoveVector.y);
-    }
-    void playerJump()
-    {
-        isJumping = true;
-        rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
-    }
-    void playerMovement()
-    {
-        xzMoveVector *= playerSpeed;
-        rb.linearVelocity = new Vector3(xzMoveVector.x, rb.linearVelocity.y, xzMoveVector.y); 
+        movementInput = context.ReadValue<Vector2>() * playerSpeed; 
+        rb.linearVelocity = new Vector3(movementInput.x, rb.linearVelocity.y, movementInput.y);
     }
     void groundCheck()
     {
@@ -88,6 +76,7 @@ public class NewPlayerMovement : MonoBehaviour
         if (hits.Length > 0)
         {
             isGrounded = true;
+            isJumping = false;
         }
         else
         {
